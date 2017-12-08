e30374e2d8b10fbf234d158e761695bb588e52a3
ElizRe <elizreed@laposte.net>
2017-12-08 15:37:28 +0100

    modified app et index files

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // good luck !
+
+$(document).ready(function(){
+
+	var buttons= $("[type='button']");
+
+		for (var i = 0; i< buttons.length; i++) { 
+
+	var indiv=$(buttons[i]); 
+	var color=indiv.attr('data-color'); 
+	indiv.css('background-color',color);
+
+	}
+
+			$(buttons).click(function(){
+		  		var color=$(this).attr('data-color');
+
+
+			if ( $("#modify-texte").is(':checked') )
+
+				{
+    		  
+					$('body').css('color',color);
+
+				}
+				
+			else { $('body').css('background-color',color);	}
+
+	});
+
+});
+
+	
+
